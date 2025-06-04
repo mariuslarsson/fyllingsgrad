81e00d30bd0b78ceb19a2edfb593d3321777cd33
marius <marius.larsson@gmail.com>
2025-06-04 15:20:31 +0200

    added cumulative perc.

--- src/createView.py
+++ src/createView.py
@@ -10,8 +10,9 @@ def createView(cursor):
         SELECT f.fyllingsgrad, f.endring_fyllingsgrad, f.omrnr, f.omrType, f.iso_aar, f.iso_uke, 
                 o.navn,
                 m.minFyllingsgrad, m.medianFyllingsGrad, m.maxFyllingsgrad,
-                n.value
-                
+                n.value,                
+                SUM(n.value) OVER (PARTITION BY f.omrnr ORDER BY f.iso_aar, f.iso_uke) AS kumulativ_nedboer
+                   
         FROM fyllingsgrad f
                 
         LEFT JOIN omraader o 
@@ -20,8 +21,9 @@ def createView(cursor):
         LEFT JOIN fyllingsgrad_maxmin m
                 ON f.omrType = m.omrType AND f.omrnr = m.omrnr AND f.iso_uke = m.iso_uke
         LEFT JOIN (
-                SELECT * FROM nedboer WHERE referenceHour = 0 AND iso_dag = 1
+                SELECT iso_aar, iso_uke, omrnr, value FROM nedboer WHERE referenceHour = 0 AND iso_dag = 1
                 ) n
                 ON f.iso_aar = n.iso_aar AND f.iso_uke = n.iso_uke AND f.omrnr = n.omrnr;
+
 
     ''')
